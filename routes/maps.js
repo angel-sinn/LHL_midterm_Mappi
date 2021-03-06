fb330eab359c43c9a8c4486a48c99a74caeab0f6
Isamu Ito <isamsito@gmail.com>
2021-03-06 21:43:06 +0000

    help me

--- routes/maps.js
+++ routes/maps.js
@@ -14,7 +14,10 @@ module.exports = (db) => {
       // console.error('error:', error); // Print the error if one occurred
       // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
       // console.log('body:', body); // Print the HTML for the Google homepage.
-      res.send(response.body);
+      const templateVars = {
+        someValue: response.body
+      }
+      res.render("index", templateVars);
 
     });
 
