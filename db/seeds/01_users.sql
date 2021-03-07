d71cb95c29cd168549ed79bb28a7a94c9c828b13
Joshua Rully <joshrully@gmail.com>
2021-03-07 23:20:30 +0000

    schema modifications for route testing purposes

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (id, name, email, password) VALUES
+(1, 'Alice', 'alice@test.com', 'password'),
+(2, 'Kira', 'kira@test.com', 'password');
--- db/seeds/03_pins.sql
+++ db/seeds/03_pins.sql
@@ -1,5 +1,5 @@
 -- TEST DATA BELOW
-INSERT INTO pins (id, title, lat, lng, address) VALUES
+INSERT INTO pins (id, map_id, title, lat, lng, address) VALUES
 
-(1, 'hoovenschmooven', 	-17.824858, 31.053028, 'Harare, Zimbabwe'),
-(1, 'ahjeez', 40.745255, -74.034775, 'Hoboken, NJ, USA');
+(1, 1, 'hoovenschmooven', 	-17.824858, 31.053028, 'Harare, Zimbabwe'),
+(2, 1, 'ahjeez', 40.745255, -74.034775, 'Hoboken, NJ, USA');
