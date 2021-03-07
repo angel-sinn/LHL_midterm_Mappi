cade99178ce39c35633b745d6ad21dfbd17fb231
Joshua Rully <joshrully@gmail.com>
2021-03-07 19:31:30 +0000

    add test seed for pins

---
+++ db/seeds/03_pins.sql
@@ -0,0 +1,5 @@
+-- TEST DATA BELOW
+INSERT INTO pins (id, title, lat, lng, address) VALUES
+
+(1, 'hoovenschmooven', 	-17.824858, 31.053028, 'Harare, Zimbabwe'),
+(1, 'ahjeez', 40.745255, -74.034775, 'Hoboken, NJ, USA');
