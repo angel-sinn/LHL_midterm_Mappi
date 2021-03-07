52e5a0e4a6610235114f35a53f94eedf3fd8ab68
Joshua Rully <joshrully@gmail.com>
2021-03-07 23:30:30 +0000

    uncomment portion of GET

--- routes/pins.js
+++ routes/pins.js
@@ -7,13 +7,9 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-<<<<<<< HEAD
     let query = "SELECT * FROM pins;";
     console.log(query);
     db.query(query)
-=======
-    db.query(`SELECT * FROM pins;`)
->>>>>>> 615462e1793a2f96a4833fd2a49d0476705d2cea
       .then(data => {
         const pins = data.rows;
         res.json({ pins });
@@ -24,17 +20,10 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
-<<<<<<< HEAD
-  // router.get('/', (req, res) => {
-  //   const templateVars = pins.coords; //example code
-  //   res.render("pins", templateVars); //separate render for pins OR index w/ pins details
-  // });
-=======
   router.get('/', (req, res) => {
     const templateVars = pins.coords; //example code
     res.render("pins", templateVars); //separate render for pins OR index w/ pins details
   });
->>>>>>> 615462e1793a2f96a4833fd2a49d0476705d2cea
   return router;
 };
 
