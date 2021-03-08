a59ab958e6174ee886ec1eecbab83aee4bd5e1cd
Joshua Rully <joshrully@gmail.com>
2021-03-08 05:20:13 +0000

    refactored seeds format

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,5 @@
--- Users table seeds here (Example)
+-- Test data below
+
 INSERT INTO users (id, name, email, password) VALUES
-(1, 'Alice', 'alice@test.com', 'password'),
-(2, 'Kira', 'kira@test.com', 'password');
+  (1, 'Alice', 'alice@test.com', 'password'),
+  (2, 'Kira', 'kira@test.com', 'password');
--- db/seeds/03_pins.sql
+++ db/seeds/03_pins.sql
@@ -1,5 +1,6 @@
 -- TEST DATA BELOW
 INSERT INTO pins (id, map_id, title, lat, lng, address) VALUES
-
-(1, 1, 'hoovenschmooven', 	-17.824858, 31.053028, 'Harare, Zimbabwe'),
-(2, 1, 'ahjeez', 40.745255, -74.034775, 'Hoboken, NJ, USA');
+  (1, 1, 'hoovenschmooven', 	-17.824858, 31.053028, 'Harare, Zimbabwe'),
+  (2, 1, 'ahjeez', 40.745255, -74.034775, 'Hoboken, NJ, USA'),
+  (3, 2, 'hoovenschmooven', 	-17.824858, 31.053028, 'Harare, Zimbabwe'),
+  (4, 2, 'ahjeez', 40.745255, -74.034775, 'Hoboken, NJ, USA');
