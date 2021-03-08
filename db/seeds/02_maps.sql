6e243dbc8f75cde10fe2869a72639e17f16655e0
Joshua Rully <joshrully@gmail.com>
2021-03-08 05:20:59 +0000

    add seed test data for maps and favourite_maps ~ nonsensical map-pin relation

---
+++ db/seeds/02_maps.sql
@@ -0,0 +1,5 @@
+INSERT INTO maps (user_id, title, category, lat, lng) VALUES
+  (1, 'the land of fun', 'entertainment', 23.806078, 11.288452),
+  (1, 'pain cave', 'hospitals', 67.496780, 64.060638),
+  (2, 'the land of fun', 'entertainment', 23.806078, 11.288452),
+  (2, 'pain cave', 'hospitals', 67.496780, 64.060638);
