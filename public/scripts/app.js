a4be70dbae600db508e4fd597452a3ff7c227156
Angel Sinn <angelkysinn@gmail.com>
2021-03-09 21:37:01 +0000

    Removed sample GET ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,6 @@
 $(() => {
   $.ajax({
     method: "GET",
-    url: "/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });
-  $.ajax({
-    method: "GET",
     url: "/pins"
   }).done((res) => {
     console.log(res);
