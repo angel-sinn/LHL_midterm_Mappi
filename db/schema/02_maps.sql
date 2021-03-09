d0438ab6188dad2c454d8928627e419b312fb7d7
Joshua Rully <joshrully@gmail.com>
2021-03-09 00:57:36 +0000

    add description to maps table

--- db/schema/02_maps.sql
+++ db/schema/02_maps.sql
@@ -5,6 +5,7 @@ CREATE TABLE maps (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INT REFERENCES users(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
+  description TEXT,
   category VARCHAR(255) DEFAULT NULL,
   lat DECIMAL (8,6),
   lng DECIMAL (9,6),
