68fbee070105bf8fb6c6e4524ae7658576c318ed
Angel Sinn <angelkysinn@gmail.com>
2021-03-10 03:13:30 +0000

    Updated ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,12 @@
 $(() => {
   $.ajax({
     method: "GET",
-    url: "/pins"
+    url: `/pins/${mapId}`
   }).done((res) => {
+    console.log('response received');
     console.log(res);
+    addMarkers(res);
   })
+
+
 });
