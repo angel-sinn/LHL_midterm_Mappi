c1031ca282ae3c01782a28d1e8687d368b820bec
Isamu Ito <isamsito@gmail.com>
2021-03-10 16:03:26 +0000

    query responds with location to redirect to, and removed some comments/console.logs

--- routes/pins.js
+++ routes/pins.js
@@ -41,36 +41,28 @@ module.exports = (db) => {
 
   // create pins for a map
   router.post("/", (req,res)=>{
-    console.log('###################################### received ajax post ###########################');
     // get all pins from saveMap in map.ejs
     let temp_query = `
     INSERT INTO pins (map_id, title, description, image_url, lat, lng, address)
     VALUES
     `;
-    // req.body.array.forEach(element => {
-    //   console.log(element);
-    // });
 
     for(let id in req.body){
       temp_query += `(${parseInt(req.body[id].map_id)},'${req.body[id].title}','${req.body[id].description}', '${req.body[id].image_url}', ${Number(req.body[id].lat)},  ${Number(req.body[id].lng)}, '${req.body[id].address}'), `;
     }
-    query = temp_query.slice(0,-2) + ' RETURNING \*;'
+    query = temp_query.slice(0,-2) + ' RETURNING *;'
 
     db.query(query)
     .then((response) => {
-      res.redirect('/')
-    })
+      map_id = response.rows[0].map_id;
+      res.send(`/maps/${map_id}`);
+    }).catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
   });
-  // delete pins for a map
-  // router.post("/:id/delete", (req, res) => {
 
-
-
-
-  // router.get('/:id', (req, res) => { // id is map_id
-  //   const templateVars = pins; //example code
-  //   res.render("pins", templateVars); //separate render for pins OR index w/ pins details
-  // });
   return router;
 };
 
