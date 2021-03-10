21a2122c40e95a56743f8ed95710bdcefa114d94
Isamu Ito <isamsito@gmail.com>
2021-03-10 19:15:45 +0000

    update pin table and seeds

--- db/schema/03_pins.sql
+++ db/schema/03_pins.sql
@@ -5,6 +5,7 @@ CREATE TABLE pins (
   -- NOT NULL below might break things
   id SERIAL PRIMARY KEY NOT NULL,
   map_id INT REFERENCES maps(id) ON DELETE CASCADE,
+  user_id INT REFERENCES users(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
   description TEXT,
   image_url VARCHAR(255),
--- db/seeds/03_pins.sql
+++ db/seeds/03_pins.sql
@@ -1,6 +1,7 @@
 -- TEST DATA BELOW
-INSERT INTO pins (map_id, title, image_url, lat, lng, address) VALUES
-  (1, 'hoovenschmooven', 'test.url', 	-28.166267, 134.2637837395668, 'Harare, Zimbabwe'),
-  (1, 'ahjeez', 'test.url', -23.258816, 125.826283, 'Hoboken, NJ, USA'),
-  (2, 'hoovenschmooven', 'test.url', 	-17.824858, 31.053028, 'Harare, Zimbabwe'),
-  (2, 'ahjeez', 'test.url', 40.745255, -74.034775, 'Hoboken, NJ, USA');
+INSERT INTO pins (map_id, user_id, title, image_url, lat, lng, address) VALUES
+  (1, 1, 'hoovenschmooven', 'test.url', 	-28.166267, 134.2637837395668, 'Harare, Zimbabwe'),
+  (1, 1, 'ahjeez', 'test.url', -23.258816, 125.826283, 'Hoboken, NJ, USA'),
+  (2, 2, 'hoovenschmooven', 'test.url', 	-17.824858, 31.053028, 'Harare, Zimbabwe'),
+  (2, 2, 'ahjeez', 'test.url', 40.745255, -74.034775, 'Hoboken, NJ, USA'),
+  (2, 1, 'user1 contrib', 'test.url', 40.845255, -74.134775, 'down and right?');
