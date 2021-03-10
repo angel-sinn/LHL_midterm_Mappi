efff55c619ad4c1d7b54884e39d347ce67443e51
Isamu Ito <isamsito@gmail.com>
2021-03-10 03:12:02 +0000

    add example routes

--- routes/pins.js
+++ routes/pins.js
@@ -6,10 +6,15 @@ const router  = express.Router();
 // GETs below
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    let query = "SELECT * FROM pins;";
+
+  // get pins for a map
+  router.get("/:id", (req, res) => {
+    let query = `
+    SELECT * FROM pins
+    WHERE map_id = $1
+    ;`;
     console.log(query);
-    db.query(query)
+    db.query(query, [req.params.id])
       .then(data => {
         const pins = data.rows;
         res.json({ pins });
@@ -21,10 +26,32 @@ module.exports = (db) => {
       });
   });
 
-  router.post("/", (req,res)=>{
+  // update pins for a map
+  router.post("/:id", (req, res) = {
 
+  })
+
+
+  // create pins for a map
+  router.post("/", (req,res)=>{
+    // get all pins from saveMap in map.ejs
+    let query = `
+    INSERT INTO pins (map_id, title, description, image_url, lat, lng, address)
+    VALUE
+    `;
+    req.body.forEach(element => {
+      console.log(element);
+      // query+= `(${element})` // propervalues here
+    });
     console.log(req.body);
   });
+
+  // delete pins for a map
+  router.post("/:id/delete", (req, res) => {
+
+
+  })
+
   // router.get('/:id', (req, res) => { // id is map_id
   //   const templateVars = pins; //example code
   //   res.render("pins", templateVars); //separate render for pins OR index w/ pins details
