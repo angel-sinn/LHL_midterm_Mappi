e7a4deb9779fff25702b5068157a164f2d395702
Angel Sinn <angelkysinn@gmail.com>
2021-03-10 06:06:30 +0000

    working state

--- routes/users.js
+++ routes/users.js
@@ -28,8 +28,26 @@ module.exports = (db) => {
 
   router.get('/:id', (req,res) =>{
     // return single user
-    
-  })
+
+      const userAlice = {
+        id: 1,
+        name: 'Alice',
+        email: 'allice@mappi.com',
+        password: 'password',
+        image: 'https://images.unsplash.com/photo-1513245543132-31f507417b26?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80',
+      }
+
+      let query = `SELECT * FROM maps WHERE user_id = $1;`;
+      return db.query(query, [req.params.id])
+      .then(response => {
+        console.log(response.rows);
+        const templateVars = {
+          user: userAlice,
+          mapData: response.rows
+        }
+        res.render("users", templateVars);
+      });
+    })
 
   return router;
 };
