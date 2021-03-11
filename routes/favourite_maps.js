3dee2d5fa2573d5c8b566579f38c6f656e07472b
Isamu Ito <isamsito@gmail.com>
2021-03-11 00:51:43 +0000

    add favourite maps handler

--- routes/favourite_maps.js
+++ routes/favourite_maps.js
@@ -7,11 +7,28 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
+    console.log('inside get request');
     let query = "SELECT * FROM favourite_maps;";
     db.query(query)
       .then(data => {
         const favourite_maps = data.rows;
         res.json({ favourite_maps });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.post("/", (req, res) => {
+    let query = `
+    INSERT INTO favourite_maps (user_id, map_id)
+    VALUES ($1, $2);
+    `;
+    return db.query(query, [parseInt(req.body.user_id), parseInt(req.body.map_id)])
+      .then(data => {
+        console.log(data)
       })
       .catch(err => {
         res
