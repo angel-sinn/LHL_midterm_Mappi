be2f56eb89a0ad145fd7570b910a4401f94b7d3e
Joshua Rully <joshrully@gmail.com>
2021-03-11 19:02:34 +0000

    demo quality seed data changes

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,5 +1,7 @@
 INSERT INTO maps (user_id, title, description, category, lat, lng) VALUES
-  (1, 'the land of fun', 'an amazing place, I swear', 'entertainment', -25.363, 131.044),
-  (1, 'pain cave', 'great times w/ communism', 'hospitals', 67.496780, 64.060638),
+  -- user
+  (1, 'Vancouver Restaurants', 'Interesting restaurants around Vancouver', 'Restaurants', 49.2827, -123.1207),
+  (1, 'Toronto Cafes', 'Hipster cafes around Toronto', 'Cafes', 43.6532, -79.3832),
+
   (2, 'the land of fun', 'an amazing place, I swear', 'entertainment', 23.806078, 11.288452),
   (2, 'pain cave', 'great times w/ communism', 'hospitals', 67.496780, 64.060638);
