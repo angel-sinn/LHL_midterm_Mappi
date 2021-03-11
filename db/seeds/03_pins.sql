fbd58a98bf7a2ac87ef158df0607ee596c5e68a1
Isamu Ito <isamsito@gmail.com>
2021-03-11 00:49:53 +0000

    add image doesn't exit

--- db/seeds/03_pins.sql
+++ db/seeds/03_pins.sql
@@ -1,7 +1,7 @@
 -- TEST DATA BELOW
 INSERT INTO pins (map_id, user_id, title, image_url, lat, lng, address) VALUES
-  (1, 1, 'hoovenschmooven', 'test.url', 	-28.166267, 134.2637837395668, 'Harare, Zimbabwe'),
-  (1, 1, 'ahjeez', 'test.url', -23.258816, 125.826283, 'Hoboken, NJ, USA'),
-  (2, 2, 'hoovenschmooven', 'test.url', 	-17.824858, 31.053028, 'Harare, Zimbabwe'),
-  (2, 2, 'ahjeez', 'test.url', 40.745255, -74.034775, 'Hoboken, NJ, USA'),
-  (2, 1, 'user1 contrib', 'test.url', 40.845255, -74.134775, 'down and right?');
+  (1, 1, 'hoovenschmooven', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png', 	-28.166267, 134.2637837395668, 'Harare, Zimbabwe'),
+  (1, 1, 'ahjeez', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png', -23.258816, 125.826283, 'Hoboken, NJ, USA'),
+  (2, 2, 'hoovenschmooven', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png', 	-17.824858, 31.053028, 'Harare, Zimbabwe'),
+  (2, 2, 'ahjeez', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png', 40.745255, -74.034775, 'Hoboken, NJ, USA'),
+  (2, 1, 'user1 contrib', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png', 40.845255, -74.134775, 'down and right?');
