4a7ddd33ec01893e2b2d5c6d0be2bbb6dccaf209
Joshua Rully <joshrully@gmail.com>
2021-03-11 19:33:40 +0000

    hardcode zoom for testing

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,7 +1,7 @@
-INSERT INTO maps (user_id, title, description, category, lat, lng) VALUES
+INSERT INTO maps (user_id, title, description, category, lat, lng, zoom) VALUES
   -- user
-  (1, 'Vancouver Restaurants', 'Interesting restaurants around Vancouver', 'Restaurants', 49.2827, -123.1207),
-  (1, 'Toronto Cafes', 'Hipster cafes around Toronto', 'Cafes', 43.6532, -79.3832),
+  (1, 'Vancouver Restaurants', 'Interesting restaurants around Vancouver', 'Restaurants', 49.2827, -123.1207, 10),
+  (1, 'Toronto Cafes', 'Hipster cafes around Toronto', 'Cafes', 43.6532, -79.3832, 10),
 
-  (2, 'the land of fun', 'an amazing place, I swear', 'entertainment', 23.806078, 11.288452),
-  (2, 'pain cave', 'great times w/ communism', 'hospitals', 67.496780, 64.060638);
+  (2, 'the land of fun', 'an amazing place, I swear', 'entertainment', 23.806078, 11.288452, 10),
+  (2, 'pain cave', 'great times w/ communism', 'hospitals', 67.496780, 64.060638, 10);
--- db/seeds/03_pins.sql
+++ db/seeds/03_pins.sql
@@ -3,9 +3,4 @@ INSERT INTO pins (map_id, user_id, title, description, image_url, lat, lng, addr
   (1, 1, 'Cardero Restaurant', 'Fine Dining Marina Restaurant', 'https://media-cdn.tripadvisor.com/media/photo-s/12/ab/ad/61/photo0jpg.jpg', 	49.291674, -123.127480, '1583 Coal Harbour Quay, Vancouver, BC V6G 3E7'),
   (1, 1, 'Black Frog Eatery','Cozy and comfy eatery', 'https://ssmscdn.yp.ca/image/resize/e9814e29-da35-49cb-8a91-af766f7b0b7a/ypui-d-mp-pic-gal-lg/black-frog-eatery-2.jpg', 49.285227, -123.108532, '108 Cambie St, Vancouver, BC V6B 2M8'),
   (1, 2, 'Dark Table', 'Blind servers dining experience', 'https://s3-media0.fl.yelpcdn.com/bphoto/6ngjBBEeA6V3qXX3BEusww/o.jpg', 49.268591, -123.164907, '2611 W 4th Ave, Vancouver, BC V6K 1P8'),
-  (1, 2, 'Gyu-Kaku Japanese BBQ', 'Japanese BBQ chain', 'https://d1ralsognjng37.cloudfront.net/d6a34492-0be9-4ffe-a5fd-32fc90428a43.jpeg', 49.263429, -123.125686, '950 W Broadway #201, Vancouver, BC V5Z 1K7'),
-
-  (2, 1, 'user1 contrib', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png', 40.845255, -74.134775, 'down and right?'),
-  (2, 1, 'user1 contrib', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png', 40.845255, -74.134775, 'down and right?'),
-  (2, 1, 'user1 contrib', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png', 40.845255, -74.134775, 'down and right?'),
-  (2, 1, 'user1 contrib', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png', 40.845255, -74.134775, 'down and right?');
+  (1, 2, 'Gyu-Kaku Japanese BBQ', 'Japanese BBQ chain', 'https://d1ralsognjng37.cloudfront.net/d6a34492-0be9-4ffe-a5fd-32fc90428a43.jpeg', 49.263429, -123.125686, '950 W Broadway #201, Vancouver, BC V5Z 1K7');
