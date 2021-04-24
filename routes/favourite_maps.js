bfdb0575892e5615ad76995db932202f62c0df79
Angel <angelkysinn@gmail.com>
2021-04-24 00:24:37 -0700

    Code cleanup

--- routes/favourite_maps.js
+++ routes/favourite_maps.js
@@ -1,43 +1,37 @@
 // All routes for Favourite Maps are defined here
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 // GETs below
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    console.log('inside get request');
+    console.log("inside get request");
     let query = "SELECT * FROM favourite_maps;";
     db.query(query)
-      .then(data => {
+      .then((data) => {
         const favourite_maps = data.rows;
         res.json({ favourite_maps });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
   router.post("/", (req, res) => {
-
     let query = `
     INSERT INTO favourite_maps (user_id, map_id)
     VALUES ($1, $2);
     `;
-    return db.query(query, [parseInt(req.body.user_id), parseInt(req.body.map_id)])
-      .then(data => {
+    return db
+      .query(query, [parseInt(req.body.user_id), parseInt(req.body.map_id)])
+      .then((data) => {
         res.sendStatus(200);
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
   return router;
 };
-
-// POSTs below
--- routes/users.js
+++ routes/users.js
@@ -5,77 +5,69 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
-
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
-
   const userAlice = {
     id: 1,
-    name: 'Alice',
-    email: 'allice@mappi.com',
-    password: 'password',
-    image: 'https://images.unsplash.com/photo-1513245543132-31f507417b26?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80',
-  }
+    name: "Alice",
+    email: "allice@mappi.com",
+    password: "password",
+    image:
+      "https://images.unsplash.com/photo-1513245543132-31f507417b26?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80",
+  };
 
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM users;`)
-      .then(data => {
+      .then((data) => {
         const users = data.rows;
         res.json({ users });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
-  router.get('/', (req,res) =>{
+  router.get("/", (req, res) => {
     res.render("index");
   });
 
-  router.get('/:id', (req,res) =>{
+  router.get("/:id", (req, res) => {
     // return single user
 
-      let query = `SELECT * FROM maps WHERE user_id = $1;`;
-      return db.query(query, [req.params.id])
-      .then(response => {
-        console.log(response.rows);
-        const templateVars = {
-          user: userAlice,
-          mapData: response.rows
-        }
-        res.render("users", templateVars);
-      });
-    })
+    let query = `SELECT * FROM maps WHERE user_id = $1;`;
+    return db.query(query, [req.params.id]).then((response) => {
+      console.log(response.rows);
+      const templateVars = {
+        user: userAlice,
+        mapData: response.rows,
+      };
+      res.render("users", templateVars);
+    });
+  });
 
-    router.get('/:id/contributed_maps', (req, res) => {
-
-      let query = `
+  router.get("/:id/contributed_maps", (req, res) => {
+    let query = `
       SELECT DISTINCT maps.title AS title, maps.id AS map_id, pins.user_id AS collaborated_maps
       FROM pins
       JOIN maps ON maps.id = map_id
       WHERE pins.user_id = $1;`;
-      return db.query(query, [req.params.id])
-      .then(response => {
-        res.json(response.rows);
-      });
-    })
+    return db.query(query, [req.params.id]).then((response) => {
+      res.json(response.rows);
+    });
+  });
 
-    router.get('/:id/favourite_maps', (req, res) => {
-
-      let query = `
+  router.get("/:id/favourite_maps", (req, res) => {
+    let query = `
       SELECT DISTINCT maps.id AS map_id, maps.title AS map_title
       FROM favourite_maps
       JOIN maps ON maps.id = map_id
       WHERE favourite_maps.user_id = $1;`;
-      return db.query(query, [req.params.id])
-      .then(response => {
-        res.json(response.rows);
-      });
-    })
+    return db.query(query, [req.params.id]).then((response) => {
+      res.json(response.rows);
+    });
+  });
 
   return router;
 };
